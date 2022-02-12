fad4d09b24323bb81502f3634521f8519dfc6b33
top802 <tatrun89@gmail.com>
2022-02-12 20:17:39 +0200

    check

--- src/top/exercises/Main.java
+++ src/top/exercises/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("start some practice with java");
+        System.out.println("start some practice with java");
 
     }
 }
